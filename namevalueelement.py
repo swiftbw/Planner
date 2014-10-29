c3d36fbe9ae7fafdcaaed62a09a1b1f63e26b867
Brian Swift <swiftbw@gmail.com>
2014-10-28 20:11:00 -0400

    latest

---
+++ namevalueelement.py
@@ -0,0 +1,15 @@
+class NameValueElement(PanedWindow):
+      def __init__(self, name, value):
+            PanedWindow.__init__(self)
+            self._label = Label(self, text=name)
+            self._label.pack(side=LEFT)
+            self._value = Entry(self)
+            self._value.pack(side=RIGHT)
+
+      def getLabel(self):
+          return _label
+
+      def getValue(self):
+          return _value
+
+      
