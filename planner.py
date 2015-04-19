985eb86e232a1bc1bf9b9be7ab5f75f6d679592c
Brian Swift <swiftbw@gmail.com>
2015-04-19 09:32:48 -0500

    Updated planner.py to use wxPython

--- planner.py
+++ planner.py
@@ -9,7 +9,9 @@ From the view menu, the resources and funding sources set-up screens can be chos
 
 
 '''
-from Tkinter import Tk, Frame, Menu, PanedWindow, Listbox, Button, Toplevel, Label, Entry, LEFT, RIGHT, TOP, BOTTOM, END
+# from Tkinter import Tk, Frame, Menu, PanedWindow, Listbox, Button, Toplevel, Label, Entry, LEFT, RIGHT, TOP, BOTTOM, END
+import wx
+import sys
 from resources import Resources
 from fundingsources import FundingSources
 from allocationset import AllocationSet
@@ -21,51 +23,77 @@ _ResourcesFileName = "PlannerResources.txt"
 _FundingSourcesFileName = "PlannerFundingSources.txt"
 _Allocations = "PlannerAllocations.txt"
 
-class Planner(Frame):
-      def __init__(self, parent):
-            Frame.__init__(self, parent)   
+class PlannerApp(wx.App):
+      def OnInit(self):
+            self._frame = PlannerFrame(None, title = "Planner")
+            self.SetTopWindow(self._frame)
+            self._frame.Show()
+
+            return True
+      
+class PlannerFrame(wx.Frame):
+      def __init__(self, parent, id=wx.ID_ANY, title="", pos = wx.DefaultPosition, size = wx.DefaultSize, style = wx.DEFAULT_FRAME_STYLE, name = "PlannerFrame"):
+            super(PlannerFrame, self).__init__(parent, id, title, pos, size, style, name)
             self._parent = parent
-            self._resourcesWindow = None
+            self._title = title
             self._resources = Resources(_plannerDataDir+_ResourcesFileName)
             self._fundingSources = FundingSources(_plannerDataDir+_FundingSourcesFileName)
             self._allocations = AllocationSet(_plannerDataDir+_Allocations)
-            self.initUI()
+
+# Create MenuBar
+            menuBar = wx.MenuBar()
+
+# Create File Menu
+            fileMenu = wx.Menu()
+            menuExit = fileMenu.Append(wx.ID_EXIT, "E&xit"," Terminate the program")
+            menuSave = fileMenu.Append(wx.ID_SAVE, "&Save", "Save Resources and Funding Sources")
+            fileMenu.AppendSeparator()
+
+
+# Create View Menu
+            viewMenu = wx.Menu()
+
+            PLN_RESOURCES = wx.NewId()
+            PLN_FUNDINGSOURCES = wx.NewId()
             
-      def initUI(self):
-            self._parent.title("Planner")
-            menubar = Menu(self._parent)
-            self._parent.config(menu=menubar)
-            fileMenu = Menu(menubar)
-            fileMenu.add_command(label="Save", command=self.onSave)
-            fileMenu.add_command(label="Exit", command=self.onExit)
-            menubar.add_cascade(label="File", menu=fileMenu)
+            menuResources = viewMenu.Append(PLN_RESOURCES, "&Resources", "Bring up Resources Panel")
+            menuFundingSources = viewMenu.Append(PLN_FUNDINGSOURCES, "F&unding Sources", "Bring up Funding Sources Panel")
 
-            viewMenu = Menu(menubar)
-            viewMenu.add_command(label="Resources", command=self.onResources)
-            viewMenu.add_command(label="Funding Sources", command=self.onFundingSources)
-            menubar.add_cascade(label="View", menu=viewMenu)
-                        
-      def onExit(self):
-            self.quit()
+            menuBar.Append(fileMenu, "&File")
+            menuBar.Append(viewMenu, "&View")
+            
+            self.SetMenuBar(menuBar)
+            self.CreateStatusBar()
 
-      def onSave(self):
+# File Menu Items
+            self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+            self.Bind(wx.EVT_MENU, self.OnSave, menuSave)
+
+# View Menu Items            
+            self.Bind(wx.EVT_MENU, self.OnResources, menuResources)
+            self.Bind(wx.EVT_MENU, self.OnFundingSources, menuFundingSources)
+            
+            self.Show(True)
+
+      def OnExit(self, e):
+            sys.exit()
+
+      def OnSave(self, e):
             self._resources.write()
             print "Resources Saved"
             self._fundingSources.write()
             print "Funding Sources Saved"
             
-      def onResources(self):
+      def OnResources(self, e):
             self._resourcesWindow = SelectionListbox(self._parent, self._resources)
 
-      def onFundingSources(self):
+      def OnFundingSources(self, e):
             self._fundingSourcesWindow = SelectionListbox(self._parent, self._fundingSources)
         
             
 def main():
-      root = Tk()
-      root.geometry("250x150+300+300")
-      app = Planner(root)
-      root.mainloop()
+      app = PlannerApp(False)
+      app.MainLoop()
 
 if __name__ == '__main__':
       main()
